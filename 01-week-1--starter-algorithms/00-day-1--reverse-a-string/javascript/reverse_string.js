7e4c5b76bda6f63dee6fb9cf44a53f37136a7a74
hiddencilantro <stevekim1109@gmail.com>
2022-06-30 17:30:44 -0400

    complete

--- 01-week-1--starter-algorithms/00-day-1--reverse-a-string/javascript/reverse_string.js
+++ 01-week-1--starter-algorithms/00-day-1--reverse-a-string/javascript/reverse_string.js
@@ -1,5 +1,10 @@
 function reverseString(str) {
-  // type your code here
+  const result = [];
+  
+  const arrOfChar = str.split("");
+  arrOfChar.forEach(char => result.unshift(char));
+
+  return result.join("");
 }
 
 if (require.main === module) {
@@ -11,9 +16,22 @@ if (require.main === module) {
 
   console.log("Expecting: 'ybabtac'");
   console.log("=>", reverseString("catbaby"));
+
+  console.log("");
+
+  console.log("Expecting: 'ortnalicneddih'");
+  console.log("=>", reverseString("hiddencilantro"));
 }
 
 module.exports = reverseString;
 
 // Please add your pseudocode to this file
+// -> create an empty array to push result
+// -> split the string into an array of characters and store it in a variable
+// -> map over it and unshift each item
+// -> join the mapped array back into a string
+
 // And a written explanation of your solution
+// -> Split the input string into an array of characters 
+// -> iterate through the array and unshift each item (add to beginning of array) into another array
+// -> join the array with the characters in the correct order back to a string and return
