49f45c4c971d0f898e5265f2240138493bdd3c63
hiddencilantro <stevekim1109@gmail.com>
2022-07-08 16:48:11 -0400

    complete

--- 01-week-1--starter-algorithms/01-day-2--find-first-duplicate/javascript/find_first_duplicate.js
+++ 01-week-1--starter-algorithms/01-day-2--find-first-duplicate/javascript/find_first_duplicate.js
@@ -1,5 +1,12 @@
 function findFirstDuplicate(arr) {
-  // type your code here
+  const uniq = new Set();
+
+  for (const value of arr) {
+    if (uniq.has(value)) return value;
+    uniq.add(value);
+  };
+
+  return -1;
 }
 
 if (require.main === module) {
@@ -11,9 +18,26 @@ if (require.main === module) {
 
   console.log("Expecting: -1");
   console.log("=>", findFirstDuplicate([1, 2, 3, 4]));
+
+  console.log("");
+
+  console.log("Expecting: 8");
+  console.log("=>", findFirstDuplicate([5, 6, 8, 10, 9, 8, 13, 14, 14]));
+
+  console.log("");
+
+  console.log("Expecting: -1");
+  console.log("=>", findFirstDuplicate([24, 28, 36]));
 }
 
 module.exports = findFirstDuplicate;
 
 // Please add your pseudocode to this file
+// 1. declare a variable and assign it to a an instance of a new Set
+// 2. iterate through the array, checking to see if each value exists in the Set
+// 3. if it does, return the value, otherwise add it to the Set
+// 4. return -1 from outmost scope
+
 // And a written explanation of your solution
+// A Set object lets you store unique values (a value can only occur once in the collection)
+// Using this characteristic, we iterate through the array, check if the value exists and add it to the collection if it doesn't.
