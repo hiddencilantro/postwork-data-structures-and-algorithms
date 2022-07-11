ced6074607b4f7815569be90255b979431990b10
hiddencilantro <stevekim1109@gmail.com>
2022-07-11 16:50:15 -0400

    complete

--- 03-week-3--additional-practice/00-bonus-1--balancing-parenetheses/javascript/balancing_parentheses.js
+++ 03-week-3--additional-practice/00-bonus-1--balancing-parenetheses/javascript/balancing_parentheses.js
@@ -1,5 +1,27 @@
 function balancingParentheses(string) {
-  // type your code here
+  // ***INITIAL ATTEMPT***
+  // const arrayOfParantheses = string.split("");
+  // let open = 0;
+  // let close = 0;
+  // arrayOfParantheses.forEach(element => element === "(" ? ++open : ++close);
+  // return Math.abs(open - close);
+  // ***DOESN'T WORK IF STRING STARTS WITH CLOSING PARANTHESIS OR ENDS WITH A CLOSING PARANTHESIS***
+
+  let open = 0;
+  let missing = 0;
+  for (let i = 0; i < string.length; ++i) {
+    if (string[i] === '(') {
+      ++open;
+      continue;
+    };
+
+    if (open > 0) {
+      --open;
+    } else {
+      ++missing;
+    };
+  };
+  return open + missing;
 }
 
 if (require.main === module) {
