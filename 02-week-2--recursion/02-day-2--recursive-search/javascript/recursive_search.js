69da8a0833defea03dbb87a9931d70974e901238
hiddencilantro <stevekim1109@gmail.com>
2022-07-11 14:59:07 -0400

    complete

--- 02-week-2--recursion/02-day-2--recursive-search/javascript/recursive_search.js
+++ 02-week-2--recursion/02-day-2--recursive-search/javascript/recursive_search.js
@@ -1,5 +1,9 @@
 function recursiveSearch(arr, target) {
   // type your code here
+  if (arr.length === 0) return false;
+  if (arr[0] === target) return true;
+  arr.shift();
+  return recursiveSearch(arr, target);
 }
 
 if (require.main === module) {
